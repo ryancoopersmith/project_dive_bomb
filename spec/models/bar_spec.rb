35543e575cdb5d7c36435b2841aed5f251f6f02b
Ryan Coopersmith <ryancoopersmith1@gmail.com>
2017-03-07 16:43:31 -0500

    Fix model spec

--- spec/models/bar_spec.rb
+++ spec/models/bar_spec.rb
@@ -1,15 +1,5 @@
 require 'rails_helper'
 
-name 'Rays Happy Birthday Bar'
-url 'www.rayshappybirthdaybar.com'
-address '123 Passyunk Ave'
-city 'Philadelphia'
-state 'PA'
-zip '19147'
-phone_number '215-215-2152'
-image_url 'nautilus_shell.jpg'
-rating '4'
-
 describe Bar do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:address) }
