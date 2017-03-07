adbcf3c0194e3ef0927e879b1d0b50325f914839
ARILANDAU <ariellelandau360@gmail.com>
2017-03-07 16:10:15 -0500

    Updated reviews

---
+++ spec/factories/reviews.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :review do
+    username 'arilandau'
+    drinks '1'
+    food '1'
+    entertainment '1'
+    vibe '1'
+    decor '1'
+    description 'What an amazing bar! What an amazing bar! What an amazing bar! What an amazing bar! What an amazing bar!'
+  end
+end
