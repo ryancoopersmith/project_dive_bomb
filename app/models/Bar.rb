9261461277f215186d6902216f86a72fd541aa3a
Corey Fischer <fischer0525@gmail.com>
2017-03-07 11:31:52 -0500

    Index MVC complete with Capybara passing tests

--- app/models/Bar.rb
+++ app/models/Bar.rb
@@ -1,9 +1,10 @@
 class Bar < ApplicationRecord
-  validates :name, presence: true
-  validates :address, presence: true, uniqueness: true
-  validates :city, presence: true
+
   validates :state, presence: true
   validates :zip, presence: true, length: { is: 5 }
   validates :phone_number, presence: true, uniqueness: true, format: { with: /\d\d\d-\d\d\d-\d\d\d\d/ }
   validates :rating, presence: true
+  validates :name, presence: true
+  validates :address, presence: true, uniqueness: true
+  validates :city, presence: true
 end
