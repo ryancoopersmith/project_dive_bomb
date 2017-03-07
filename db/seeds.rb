699abfd5e5561677234fcfb91f33e072be8c7cff
Ryan Coopersmith <ryancoopersmith1@gmail.com>
2017-03-07 12:01:45 -0500

    Add Files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Rails.env.development? || Rails.env.production?
+  devin = User.create!(username: "dbombay", email: "devin.bombay@gmail.com",
+   password: "dbombay1", password_confirmation: "dbombay1")
+  ryan = User.create!(username: "ryancoopersmith1", email: "ryancoopersmith1@gmail.com",
+   password: "password", password_confirmation: "password")
+  arielle = User.create!(username: "arilandau", email: "ariellelandau360@gmail.com",
+   password: "password1", password_confirmation: "password1")
+  corey = User.create!(username: "fischer0525", email: "fischer0525@gmail.com",
+   password: "admin", password_confirmation: "admin")
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,6 @@ FactoryGirl.define do
     username 'johnBombbb'
     password 'password'
     password_confirmation 'password'
+    admin false
   end
 end
