bfdeac6b8c2df1d9b09373cdf4981bcd340b659f
Ryan Coopersmith <ryancoopersmith1@gmail.com>
2017-03-07 14:01:37 -0500

    Add admin feature

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -6,4 +6,12 @@ class BarsController < ApplicationController
   def show
     @bar = Bar.find(params[:id])
   end
+
+  protected
+
+  def authorize_user
+    if !user_signed_in? || !current_user.admin?
+      raise ActionController::RoutingError.new("Not Found")
+    end
+  end
 end
