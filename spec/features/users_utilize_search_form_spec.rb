811fb981d10aad70d926adb8a48a98f2f7473c74
DBombay <d.bombay@live.com>
2017-03-08 16:13:10 -0500

    Edits for hound and refactored test

--- spec/features/users_utilize_search_form_spec.rb
+++ spec/features/users_utilize_search_form_spec.rb
@@ -5,15 +5,14 @@ feature "visitor sees search bar on index page" do
     visit root_path
 
     expect(page).to have_button('Search')
-
-
   end
 end
 
 feature "visitor interacts with search bar" do
+  let bar1 = FactoryGirl.create(:bar)
+  let bar2 = FactoryGirl.create(:bar)
+
   scenario "visitor does not use search function" do
-    bar1 = FactoryGirl.create(:bar)
-    bar2 = FactoryGirl.create(:bar)
     visit root_path
 
     expect(page).to have_content bar1.name
@@ -23,9 +22,7 @@ feature "visitor interacts with search bar" do
 
   scenario "visitor searches for bar1, and doesn't see bar2" do
     visit root_path
-    bar1 = FactoryGirl.create(:bar)
-    bar2 = FactoryGirl.create(:bar)
-    
+
     fill_in "term", with: bar1.name
     click_button 'Search'
 
