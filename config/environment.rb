70c580bd988ca5f93e4c8b8cbca36d5c979e322a
Ryan Coopersmith <ryancoopersmith1@gmail.com>
2017-03-09 11:21:18 -0500

    fix hound errors

--- config/environment.rb
+++ config/environment.rb
@@ -6,12 +6,12 @@ Rails.application.initialize!
 
 if Rails.env.production?
   ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'https://frozen-wave-13196.herokuapp.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'https://frozen-wave-13196.herokuapp.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
   }
 end
