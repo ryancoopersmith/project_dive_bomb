e9d2e5d202c30c05a1b755a1d517fc9fb49bc028
Ryan Coopersmith <ryancoopersmith1@gmail.com>
2017-03-08 19:17:42 -0500

    fix sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,14 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.active_record.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'https://frozen-wave-13196.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
 end
