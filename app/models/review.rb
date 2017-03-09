6145d16203a6d8e80fa605638e4d527b85b3b0dd
ARILANDAU <ariellelandau360@gmail.com>
2017-03-09 13:22:15 -0500

    Fix hound violations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,11 +1,21 @@
 class Review < ApplicationRecord
   validates :user, presence: true
   validates :bar, presence: true
-  validates :drinks, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
-  validates :food, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
-  validates :entertainment, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
-  validates :vibe, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
-  validates :setting, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  validates :drinks, presence: true, numericality:
+    { only_integer: true, greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 5 }
+  validates :food, presence: true, numericality:
+    { only_integer: true, greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 5 }
+  validates :entertainment, presence: true, numericality:
+    { only_integer: true, greater_than_or_equal_to: 1,
+      less_than_or_equal_to: 5 }
+  validates :vibe, presence: true, numericality:
+    { only_integer: true, greater_than_or_equal_to: 1,
+      less_than_or_equal_to: 5 }
+  validates :setting, presence: true, numericality:
+    { only_integer: true, greater_than_or_equal_to: 1,
+      less_than_or_equal_to: 5 }
 
   belongs_to :bar
   belongs_to :user
