c2d6479946d6754e29ee6076e1caa0c08ff2550e
Ryan Coopersmith <ryancoopersmith1@gmail.com>
2017-03-08 19:21:10 -0500

    fix sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActiveMailer::Base.smtp_settings = {
+ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'https://frozen-wave-13196.herokuapp.com',
