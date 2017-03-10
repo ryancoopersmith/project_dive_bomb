44733f63f80e418a6a30ecf0ef1685fb9f279fcc
Corey Fischer <fischer0525@gmail.com>
2017-03-10 14:16:31 -0500

    Most tests and form corrections

--- spec/features/admin_creates_bar_spec.rb
+++ spec/features/admin_creates_bar_spec.rb
@@ -11,11 +11,70 @@ feature 'create a bar link is only available to admin' do
 
     expect(page).to_not have_link "Create Bar"
   end
+
+  scenario "Authorized user visits homepage and does not see button to create bar" do
+    user
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Sign In'
+
+    expect(page).to_not have_link "Create Bar"
+  end
+
+  scenario "Admin visits homepage and sees button to create bar" do
+    admin
+    visit new_user_session_path
+    fill_in 'Email', with: admin.email
+    fill_in 'Password', with: admin.password
+    click_button 'Sign In'
+
+    expect(page).to have_link "Create Bar"
+  end
 end
 
-#User tries to go direct to new path
-#Unauthorized users tries "
-#Admin tries to do this thing
+feature 'create a bar link is only available to admin' do
+  let(:user) {FactoryGirl.create(:user)}
+  let(:admin) { FactoryGirl.create(:user, admin: true) }
+
+  scenario "Unauthorized user is unable to use direct path to new bar form" do
+    visit new_bar_path
+
+    expect { new_bar_path }.to raise_error(ActionController::RoutingError, "Not Found")
+    #not passing, rake is bringing up an older format and won't reset to the new
+  end
+
+  scenario "Authorized user is unable to use direct path to new bar form" do
+    user
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Sign In'
+    visit new_bar_path
+
+    expect { new_bar_path }.to raise_error(ActionController::RoutingError, "Not Found")
+  end
+
+  scenario "Admin is able to use direct path to new bar form" do
+    admin
+    visit new_user_session_path
+    fill_in 'Email', with: admin.email
+    fill_in 'Password', with: admin.password
+    click_button 'Sign In'
+    visit new_bar_path
+
+    expect(page).to have_content('Name')
+    expect(page).to have_content('Address')
+    expect(page).to have_content('City')
+    expect(page).to have_content('State')
+    expect(page).to have_content('Zip')
+    expect(page).to have_content('Phone Number')
+    expect(page).to have_content('Image Url')
+    expect(page).to have_content('Url')
+    expect(page).to have_content('Description')
+  end
+end
+
 
 #Admin creates new bar
 
