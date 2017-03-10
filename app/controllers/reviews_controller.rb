25801fe917eebf858bca4e8a7d4ec0213ef2368f
Ryan Coopersmith <ryancoopersmith1@gmail.com>
2017-03-10 10:07:56 -0500

    Add ESLint

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,16 @@
 class ReviewsController < ApplicationController
+  def create
+    @review = Review.new(reviews_params)
+    if @review.save
+    end
+  end
+
+  def update
+  end
+
+  private
+
+  def reviews_params
+    params.require(:review).permit(:drinks, :food, :entertainment, :vibe, :setting, :description, :user_id, :bar_id, :votes)
+  end
 end
