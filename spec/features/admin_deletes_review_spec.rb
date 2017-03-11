85a54c64312523b619b262bbba41d3577c7c662a
Ryan Coopersmith <ryancoopersmith1@gmail.com>
2017-03-11 17:50:23 -0500

    Add admin deletes review spec

---
+++ spec/features/admin_deletes_review_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+# As an admin
+# I would like to delete user's reviews
+# So that I can filter any inappropriate content
+
+# [] I should be able to delete any review
+# [] I should see a success message upon successful deletion
+
+feature "Admin deletes review" do
+  let!(:admin) { FactoryGirl.create(:user, admin: true) }
+  let!(:user) { FactoryGirl.create(:user) }
+  let!(:bar) { FactoryGirl.create(:bar) }
+  let!(:review) { FactoryGirl.create(:review, user: user, bar: bar) }
+
+  scenario "admin clicks delete link" do
+    visit root_path
+    click_link "Sign In"
+    fill_in 'Email', with: admin.email
+    fill_in 'Password', with: admin.password
+    click_button "Sign In"
+    visit bar_path(bar)
+    click_link "Delete Review"
+    expect(page).to have_content("Review deleted successfully")
+    expect(page).to_not have_content(review.description)
+  end
+end
