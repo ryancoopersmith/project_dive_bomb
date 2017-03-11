7cbe83916611a52769e54725ce2341e6d50cb9e0
Ryan Coopersmith <ryancoopersmith1@gmail.com>
2017-03-11 11:45:32 -0500

    Add form for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,14 @@
 class ReviewsController < ApplicationController
+  def new
+    unless current_user.reviews
+      @bar = Bar.find(params[:id])
+      @review = Review.new
+    else
+      flash[:notice] = "You cannot submit more than one review"
+      render 'bars/show'
+    end
+  end
+
   def create
     @review = Review.new(reviews_params)
     @bar = @review.bar
@@ -10,6 +20,9 @@ class ReviewsController < ApplicationController
     render 'bars/show'
   end
 
+  def edit
+  end
+
   def update
     @review = Review.assign_attributes(reviews_params)
     @bar = review.bar
@@ -18,6 +31,13 @@ class ReviewsController < ApplicationController
     else
       flash[:notice] = @review.errors.messages
     end
+    render 'bars/show'
+  end
+
+  def destroy
+    @review = Review.find_by(user: current_user)
+    @review.destroy
+    flash[:notice] = "Review deleted successfully"
     render 'bars/show'
   end
 
