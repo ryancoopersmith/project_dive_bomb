09668162c53a6e5b5acbf65b4c7d54cce6eeec5e
Ryan Coopersmith <ryancoopersmith1@gmail.com>
2017-03-12 13:13:04 -0400

    Add account functionality

---
+++ spec/features/user_visits_account_spec.rb
@@ -0,0 +1,9 @@
+# As an authenticated user
+# I would like to see my account information
+# So that I can change any information I don't like
+
+# [] I must be signed in
+# [] I should see my profile pic
+# [] I should be able to change my profile pic
+# [] I should be able to delete my account
+# [] I should be able to reset my password
