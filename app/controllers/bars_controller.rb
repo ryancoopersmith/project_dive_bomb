b766e1838d9a13c2496be51700219f1ac3231043
Ryan Coopersmith <ryancoopersmith1@gmail.com>
2017-03-13 14:10:44 -0400

    fix hound error

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -14,10 +14,10 @@ class BarsController < ApplicationController
         sum += (review.drinks + review.food + review.entertainment + review.vibe + review.setting)
       end
     end
-    if sum < 1
-      @user_avg_rating = "No user has reviewed this dive yet"
+    @user_avg_rating = if sum < 1
+      "No user has reviewed this dive yet"
     else
-      @user_avg_rating = sum / 5.0
+      sum / 5.0
     end
   end
 
