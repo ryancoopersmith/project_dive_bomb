02dc8a5bd5b109307e7f3f57bae40daf9bea34ab
Ryan Coopersmith <ryancoopersmith1@gmail.com>
2017-03-13 16:58:27 -0400

    Write controller specs

---
+++ spec/controllers/bars_api_spec.rb
@@ -0,0 +1,17 @@
+# require 'rails_helper'
+#
+# describe Api::V1::BarsController do
+#   let(:bar) { FactoryGirl.create!(:bar) }
+#   let(:admin) { FactoryGirl.create!(:user, admin: true) }
+#
+#   it 'should be successful' do
+#
+#     response.should be_success
+#   end
+# 
+#   it 'responds with JSON' do
+#
+#     body = JSON.parse(response.body)
+#     expect(body[0]['name']).to eq(bar.name)
+#   end
+# end
