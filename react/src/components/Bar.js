17f1d61d0f5c0018fb0a30d189701b0926534970
Ryan Coopersmith <ryancoopersmith1@gmail.com>
2017-03-14 16:17:33 -0400

    Add conditional class in React

--- react/src/components/Bar.js
+++ react/src/components/Bar.js
@@ -3,17 +3,38 @@ import React, { Component } from 'react';
 class Bar extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      row: ''
+    };
+    this.setRow = this.setRow.bind(this);
+  }
+
+  setRow(id) {
+    if (id % 8 === 0) {
+      this.setState({ row: 'row' });
+    } else if (id % 8 === 1){
+      this.setState({ row: '' });
+    }
+  }
+
+  componentDidMount() {
+    this.setRow(this.props.id)
   }
 
   render() {
+
     return (
-      <div className="card">
-        <div className="card-divider">
-          <img src={this.props.image_url} className="image" />
-          <div className="card-section">
-            <p> {this.props.rating} </p>
-            <p> {this.props.name}  </p>
-            <p id="view_bar"><a href={`bars/${this.props.id}`}>View Bar</a></p>
+      <div className={this.state.row}>
+        <div className="card medium-4 columns">
+          <div className="card-divider">
+            <img src={this.props.image_url} className="image" />
+            <div className="card-section">
+              <div className="info">
+                <p> {this.props.name}  </p>
+                <p> Rating: {this.props.rating} </p>
+                <a href={`bars/${this.props.id}`} className="view">View Bar</a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
