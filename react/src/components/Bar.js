adbd492917baada8f43f8d510b156565d804b747
Ryan Coopersmith <ryancoopersmith1@gmail.com>
2017-03-14 20:57:03 -0400

    Fix grid

--- react/src/components/Bar.js
+++ react/src/components/Bar.js
@@ -3,37 +3,25 @@ import React, { Component } from 'react';
 class Bar extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      row: ''
-    };
-    this.setRow = this.setRow.bind(this);
-  }
-
-  setRow(id) {
-    if (id % 8 === 0) {
-      this.setState({ row: 'row' });
-    } else if (id % 8 === 1){
-      this.setState({ row: '' });
-    }
-  }
-
-  componentDidMount() {
-    this.setRow(this.props.id)
   }
 
   render() {
+    let classNames = require('classnames');
+    let gridClasses = classNames({
+      'card': true,
+      'medium-4': true,
+      'columns': true
+    })
 
     return (
-      <div className={this.state.row}>
-        <div className="card medium-4 columns">
-          <div className="card-divider">
-            <img src={this.props.image_url} className="image" />
-            <div className="card-section">
-              <div className="info">
-                <p> {this.props.name}  </p>
-                <p> Rating: {this.props.rating} </p>
-                <a href={`bars/${this.props.id}`} className="view">View Bar</a>
-              </div>
+      <div className={gridClasses}>
+        <div className="card-divider">
+          <img src={this.props.image_url} className="image" />
+          <div className="card-section">
+            <div className="info">
+              <p> {this.props.name}  </p>
+              <p> Rating: {this.props.rating} </p>
+              <a href={`bars/${this.props.id}`} className="view">View Bar</a>
             </div>
           </div>
         </div>
