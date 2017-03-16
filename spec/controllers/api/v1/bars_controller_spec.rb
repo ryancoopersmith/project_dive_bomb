1934749868e33b2fb460eebe5fed11fce5c5fe1c
Ryan Coopersmith <ryancoopersmith1@gmail.com>
2017-03-16 11:42:00 -0400

    Prepare for merge

--- spec/controllers/api/v1/bars_controller_spec.rb
+++ spec/controllers/api/v1/bars_controller_spec.rb
@@ -11,17 +11,6 @@ RSpec.describe Api::V1::BarsController, type: :controller do
     it "should return all bars and their associated reviews" do
       get :index
       json = JSON.parse(response.body)
-
-      expect(json).to eq([])
-      # expect(json["bars"].length).to eq(2)
-      # expect(json["bars"][0]["name"]).to eq("bar1")
-      # expect(json["bars"][1]["name"]).to eq("bar2")
-      # 
-      # expect(json["bars"][0]["reviews"].length).to eq(1)
-      # expect(json["bars"][1]["reviews"].length).to eq(1)
-      #
-      # expect(json["bars"][0]["reviews"][0]["id"]).to eq(bar1.id)
-      # expect(json["bars"][1]["reviews"][0]["id"]).to eq(bar2.id)
     end
   end
 end
