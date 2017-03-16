c4959eec082b52a3541e1a97a42be7824aa6b46e
Ryan Coopersmith <ryancoopersmith1@gmail.com>
2017-03-15 22:23:36 -0400

    Set up for heroku

--- react/src/components/BarList.js
+++ react/src/components/BarList.js
@@ -24,7 +24,7 @@ class BarList extends Component {
   }
 
   getBars() {
-    fetch('http://localhost:3000/api/v1/bars.json') // https://frozen-wave-13196.herokuapp.com/api/v1/bars.json if deployed on heroku
+    fetch('https://frozen-wave-13196.herokuapp.com/api/v1/bars.json') // https://frozen-wave-13196.herokuapp.com/api/v1/bars.json if deployed on heroku http://localhost:3000/api/v1/bars.json for development
       .then(response => {
         if (response.ok) {
           return response;
