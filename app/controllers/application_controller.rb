17513930ca280bac43291d0ae83384973dd1dc34
Ryan Coopersmith <ryancoopersmith1@gmail.com>
2017-03-16 16:33:54 -0400

    Preflight check failed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  after_filter :cors_set_access_control_headers
-  before_filter :cors_preflight_check
-
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = '*'
-    headers['Access-Control-Max-Age'] = "1728000"
-  end
-
-  def cors_preflight_check
-    if request.method == :options
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, GET, DELETE, PUT, OPTIONS'
-      headers['Access-Control-Allow-Headers'] = '*'
-      headers['Access-Control-Request-Method'] = '*'
-      render :text => '', :content_type => 'text/plain'
-    end
-  end
 
   protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
     resources :reviews
   end
 
-  match "*all" => "application#cors_preflight_check", :constraints => { :method => "OPTIONS" }
-
   root 'bars#index'
 
   namespace :api do
