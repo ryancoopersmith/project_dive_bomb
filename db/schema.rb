0eaa8970b988529547df3e2a8060d94e5d51da58
Ryan Coopersmith <ryancoopersmith1@gmail.com>
2017-03-17 19:08:08 -0400

    Fix schema

---
+++ db/schema.rb
@@ -0,0 +1,64 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20170313145104) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "bars", force: :cascade do |t|
+    t.string "name",         null: false
+    t.string "url"
+    t.string "address",      null: false
+    t.string "city",         null: false
+    t.string "state",        null: false
+    t.string "zip",          null: false
+    t.string "phone_number", null: false
+    t.string "image_url"
+    t.float  "rating"
+    t.text   "description"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "drinks",        null: false
+    t.integer "food",          null: false
+    t.integer "entertainment", null: false
+    t.integer "vibe",          null: false
+    t.integer "setting",       null: false
+    t.text    "description"
+    t.integer "user_id"
+    t.integer "bar_id"
+    t.index ["bar_id"], name: "index_reviews_on_bar_id", using: :btree
+    t.index ["user_id"], name: "index_reviews_on_user_id", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username",                               null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,     null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.string   "profile_photo"
+    t.boolean  "admin",                  default: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+end
